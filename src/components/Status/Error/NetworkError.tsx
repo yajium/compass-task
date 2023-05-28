6b366109c8f3044459d0d979dc575ac6060f89b4
yajium <yajiuma720@gmail.com>
2023-05-28 15:51:16 +0900

    fix: fix network error components (add modal)

--- src/components/Status/Error/NetworkError.tsx
+++ src/components/Status/Error/NetworkError.tsx
@@ -1,14 +1,69 @@
-export default function Error({ retry }: { retry: () => void }) {
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
+
+export default function NetworkErrorModal({ retry }: { retry: () => void }) {
+  const [isOpen, setIsOpen] = useState(true);
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
-    <div>
-      <p>通信エラーが発生しました。</p>
-      <button
-        type="button"
-        onClick={retry}
-        className="my-4 rounded-md bg-blue-500 p-3 text-white"
-      >
-        リトライ
-      </button>
-    </div>
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={closeModal}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Title
+                  as="h3"
+                  className="text-lg font-medium leading-6 text-gray-900"
+                >
+                  通信エラーが発生しました。
+                </Dialog.Title>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">
+                    リトライボタンから再度お試しください。
+                  </p>
+                </div>
+
+                <div className="mt-4">
+                  <button
+                    type="button"
+                    className="my-4 rounded-md bg-blue-500 p-3 text-white hover:bg-blue-400 focus:outline-none"
+                    onClick={() => {
+                      setIsOpen(false);
+                      retry();
+                    }}
+                  >
+                    リトライ
+                  </button>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
   );
 }
